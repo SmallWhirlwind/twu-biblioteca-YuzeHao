2ce9d202ad040a68d7e9297a53c0ebca72a787f9
Yuze Hao <yzehao@CNyzehao.local>
2017-07-30 22:59:51 +0800

    [TWU_BibliotecaApp]: add print menu function - Yuze

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,18 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private final static String WELCOME = "Welcome!";
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        printMenu();
+    }
+
+    private static void printMenu() {
+        String[] menu = {"List of books", "Exit"};
+        System.out.println(WELCOME);
+        for (int index = 0; index < menu.length; index++) {
+            System.out.println(index + " - " + menu[index]);
+        }
     }
 }
